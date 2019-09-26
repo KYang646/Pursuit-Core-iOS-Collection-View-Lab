1a1e89655add04d88dc2aa80dd97074800a86dcc
Kimball Yang <kimball.yang@gmail.com>
2019-09-26 18:33:25 -0400

    initial commit

---
+++ CollectionLab/CollectionLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionLab
+//
+//  Created by Kimball Yang on 9/26/19.
+//  Copyright © 2019 Kimball Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
